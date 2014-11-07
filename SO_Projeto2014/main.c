d227d83c9249a2f862566b87af1ba500903a20e8
Miguel Diogo <MiguelDiogo@Miguels-MacBook-Air.lan>
2014-11-07 16:43:41 +0000

    Initial Commit

---
+++ SO_Projeto2014/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SO_Projeto2014
+//
+//  Created by Miguel Diogo on 07/11/14.
+//  Copyright (c) 2014 Miguel Diogo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
